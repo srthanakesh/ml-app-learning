994456906986610632fe450c2de334d52305d3f1
srthanakesh <129877578+srthanakesh@users.noreply.github.com>
2025-08-26 21:36:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,19 @@ import numpy as np
 st.title('🖥️ Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('penguins_cleaned.csv')
+  df
+  st.write("**X**")
+  X = df.drop('species',axis=1)
+  X
 
-df = pd.read_csv('penguins_cleaned.csv')
-df
+  st.write('**y**')
+  y = df.species
+  y
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  
+
