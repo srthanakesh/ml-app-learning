798089a1ab59e8ce97e2a5c010c82684bf341dd6
srthanakesh <129877578+srthanakesh@users.noreply.github.com>
2025-08-26 21:29:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🖥️ Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-df = pd.read_csv('/penguins_cleaned.csv')
+df = pd.read_csv('penguins_cleaned.csv')
 df
