6ba1ca6fd2c27a9e9bed4f7b504e4dbb8e00ba2d
srthanakesh <129877578+srthanakesh@users.noreply.github.com>
2025-08-26 21:46:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,26 @@ with st.expander('Data'):
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-  
 
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe','Dream','Torgersen'))  
+  bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6,43.9)
+  bill_depth_mm = st.slider('Bill Depth (mm)',13.1,21.5,17.2)
+  flipper_length_mm = st.slider('Flipper Length (mm)',172.0,231.0,201.0)
+  body_mass_g = st.slider('Body Mass (g)',2700.00,6300.0,4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+
+  data = {
+    'island',island,
+    'bill_length_mm', bill_length_mm,
+    'bill_depth_mm',bill_depth_mm,
+    'flipper_length_mm',flipper_length_mm,
+    'body_mass_g',body_mass_g,
+    'gender ',gender  
+  }
+  input_df = pd.DataFrame(data, index[0])
+  input_df
+
+  
+                        
